247149ac3d2939196eeb8d01417cfd40644431ef
Ben <benjaminfkile@gmail.com>
2026-04-02 19:37:15 -0500

    fix tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: ['<rootDir>/__tests__/smoke/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
